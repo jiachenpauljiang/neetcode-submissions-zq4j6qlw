90193f34f1cfa2acfea6bdd2ea80f8aba4a18a2b
Paul Jiang <jiangjc0726@gmail.com>
2026-04-24 20:27:34 -0700

    Add: binary-subarrays-with-sum - submission-2

---
+++ Data Structures & Algorithms/binary-subarrays-with-sum/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int curSum = 0;
+            for (int j = i; j < nums.length; j++) {
+                curSum += nums[j];
+                if (curSum == goal) {
+                    res += 1;
+                }
+            }
+        }
+        return res;
+    }
+}
