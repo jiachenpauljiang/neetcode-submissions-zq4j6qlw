e18c1a362d3c71ba9da9faae713a7814ee452cb6
Paul Jiang <jiangjc0726@gmail.com>
2026-06-02 17:54:57 -0700

    Add: single-element-in-a-sorted-array - submission-9

---
+++ Data Structures & Algorithms/single-element-in-a-sorted-array/submission-9.py
@@ -0,0 +1,18 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l < r:
+            m = l + (r - l) // 2
+
+            # force m to be even so that we can compare 
+            if m % 2!= 0:
+                m -= 1
+            
+            if nums[m] == nums[m + 1]:
+                # the single element is in the right half, update l
+                l = m + 2
+            else:
+                r = m
+        
+        return nums[l]
