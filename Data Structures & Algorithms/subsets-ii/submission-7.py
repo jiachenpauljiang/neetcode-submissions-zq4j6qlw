33402e14d65fd921d578938b1e1f911937676539
Paul Jiang <jiangjc0726@gmail.com>
2026-06-07 08:15:17 -0700

    Add: subsets-ii - submission-7

---
+++ Data Structures & Algorithms/subsets-ii/submission-7.py
@@ -0,0 +1,25 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        """
+        First sort the nums array so that we can check duplicates
+        Then for each number we have 2 options, use it or not use it
+        """
+        res = []
+        nums.sort()
+
+        def backtrack(start, cur_subset):
+            if start == len(nums):
+                res.append(cur_subset.copy())
+                return 
+
+            cur_subset.append(nums[start])
+            backtrack(start + 1, cur_subset)
+            cur_subset.pop()
+
+            while start + 1 < len(nums) and nums[start + 1] == nums[start]:
+                start += 1
+
+            backtrack(start + 1, cur_subset)
+        
+        backtrack(0, [])
+        return res 
