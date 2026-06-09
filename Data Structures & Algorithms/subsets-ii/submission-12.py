b1f5c97fe52bc26fc96abbd26b2dc44966490d5f
Paul Jiang <jiangjc0726@gmail.com>
2026-06-08 20:55:14 -0700

    Add: subsets-ii - submission-12

---
+++ Data Structures & Algorithms/subsets-ii/submission-12.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+
+        res = []
+
+        def dfs(start, cur):
+            res.append(cur.copy())
+
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i - 1]:
+                    # de-dup
+                    continue
+
+                cur.append(nums[i])
+                dfs(i + 1, cur)
+                cur.pop()
+        
+        dfs(0, [])
+        return res
